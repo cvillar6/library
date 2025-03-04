58838ecf82bdd20d8e158880a90d3eb062297e6c
cvilla-lealco <cvilla@leal.co>
2025-03-03 19:52:10 -0500

    🏗️ feat: Delete book added

--- backend/internal/book/delete_book.go
+++ backend/internal/book/delete_book.go
@@ -1,1 +1,9 @@
 package book
+
+func DeleteBook(book *Book, library *[]Book) {
+	for bookIndex, bookValue := range *library {
+		if bookValue.ID == book.ID {
+			*library = append((*library)[:bookIndex], (*library)[bookIndex+1:]...)
+		}
+	}
+}
